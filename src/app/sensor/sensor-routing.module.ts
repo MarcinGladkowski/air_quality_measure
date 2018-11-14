6933b59c70a489da562c1edb48f93a863b6ab39a
Gladkowski <marcingladkowski@gmail.com>
2018-11-14 22:31:16 +0100

    add routing to sensor module

--- src/app/sensor/sensor-routing.module.ts
+++ src/app/sensor/sensor-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChartComponent } from './chart/chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'chart', component: ChartComponent},
+  {path: '', redirectTo: 'chart', pathMatch: 'full'},
+  {path: '**', redirectTo: 'sensor', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
