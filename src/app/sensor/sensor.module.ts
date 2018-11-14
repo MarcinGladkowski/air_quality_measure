014df1fc3bc5dee86b1ba2d5f9e4c0b0e5cc9c71
Gladkowski <marcingladkowski@gmail.com>
2018-11-14 22:31:45 +0100

    add imports to sensor module

--- src/app/sensor/sensor.module.ts
+++ src/app/sensor/sensor.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SensorRoutingModule } from './sensor-routing.module';
+import { ChartComponent } from './chart/chart.component';
+import { SensorComponent } from './sensor/sensor.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ChartComponent, SensorComponent],
   imports: [
     CommonModule,
     SensorRoutingModule
