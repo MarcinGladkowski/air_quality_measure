a5b1ab8f956ec768b6a257eadf38e33472e7ec21
Gladkowski <marcingladkowski@gmail.com>
2018-11-14 22:30:35 +0100

    add main routes to app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { SensorModule } from './sensor/sensor.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'sensor', loadChildren: () => SensorModule },
+  {path: '', redirectTo: 'sensor', pathMatch: 'full'},
+  {path: '**', redirectTo: 'sensor', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
