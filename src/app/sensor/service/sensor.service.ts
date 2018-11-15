c2b9c4c2d2835a058d8428383442120da523e7b5
Gladkowski <marcingladkowski@gmail.com>
2018-11-15 23:06:18 +0100

    method to get data

--- src/app/sensor/service/sensor.service.ts
+++ src/app/sensor/service/sensor.service.ts
@@ -1,9 +1,21 @@
+import { environment } from './../../../environments/environment';
+import { SensorData } from './../interface/sensor-data';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Sensor } from '../model/sensor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SensorService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSensorData(): Observable<Sensor[]> {
+    return this.http.get<SensorData[]>(`${environment.apiUrl}/api/sensor`).pipe(
+      map((data) => data.map((sensorData) => new Sensor(sensorData)))
+    );
+  }
 }
