ce4ecb022e0945ac3a8a6cea17634bd7e005a6ef
Marcin Gładkowski <mgladkowski@swift.pl>
2018-11-16 16:57:56 +0100

    add simple chart to component

--- src/app/sensor/chart/chart.component.ts
+++ src/app/sensor/chart/chart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SensorService } from '../service/sensor.service';
 import { Sensor } from '../model/sensor';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-chart',
@@ -11,14 +12,51 @@ export class ChartComponent implements OnInit {
 
   p1 = [];
   date = [];
+  chart = [];
+  i = 0;
 
   constructor(private sensorService: SensorService) { }
 
   ngOnInit() {
     this.sensorService.getSensorData().subscribe((data: Sensor[]) => {
-      for (const sensor of data) {
-        this.p1.push(sensor.p1);
+
+      if (this.i <= 10) {
+        for (const sensor of data) {
+          const sensorDate: string = sensor.createdDate;
+          this.p1.push(sensor.p1);
+          this.date.push(new Date(sensorDate));
+          this.i++;
+        }
       }
+
+      this.chart = new Chart('canvas', {
+        type: 'line',
+        data: {
+          labels: this.date,
+          datasets: [
+            {
+              data: this.p1,
+              borderColor: '#3cba9f',
+              fill: false
+            }
+          ]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [{
+              display: true
+            }],
+            yAxes: [{
+              display: true
+            }],
+          }
+        }
+      });
+
+
     });
   }
 
