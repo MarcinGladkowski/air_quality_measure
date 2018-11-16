e1007b5a673a6d5ec756555e8cde6e1168093cb8
Gladkowski <marcingladkowski@gmail.com>
2018-11-17 00:18:26 +0100

    add charts for humi and temp

--- src/app/sensor/chart/chart.component.ts
+++ src/app/sensor/chart/chart.component.ts
@@ -11,8 +11,13 @@ import { Chart } from 'chart.js';
 export class ChartComponent implements OnInit {
 
   p1 = [];
+  p2 = [];
+  temp = [];
   date = [];
-  chart = [];
+  humi = [];
+  pmChart = [];
+  tempChart = [];
+  humiChart = [];
   i = 0;
 
   constructor(private sensorService: SensorService) { }
@@ -20,34 +25,142 @@ export class ChartComponent implements OnInit {
   ngOnInit() {
     this.sensorService.getSensorData().subscribe((data: Sensor[]) => {
 
-      if (this.i <= 10) {
+        data.reverse();
+
         for (const sensor of data) {
           const sensorDate: string = sensor.createdDate;
           this.p1.push(sensor.p1);
+          this.p2.push(sensor.p2);
+          this.humi.push(sensor.humi);
+          this.temp.push(sensor.temp);
           this.date.push(new Date(sensorDate));
-          this.i++;
         }
-      }
 
-      this.chart = new Chart('canvas', {
+      this.pmChart = new Chart('pmCanvas', {
         type: 'line',
         data: {
           labels: this.date,
           datasets: [
             {
               data: this.p1,
-              borderColor: '#3cba9f',
-              fill: false
+              borderColor: '#8e5ea2',
+              fill: false,
+              label: 'PM 2.5'
+            },
+            {
+              data: this.p2,
+              borderColor: '#3e95cd',
+              fill: false,
+              label: 'PM 10'
+            },
+          ]
+        },
+        options: {
+          legend: {
+            display: true,
+            position: 'right'
+          },
+          scales: {
+            xAxes: [{
+              type: 'time',
+              time: {
+                displayFormats: {
+                  'millisecond': 'h:mm',
+                  'second': 'h:mm',
+                  'minute': 'h:mm',
+                  'hour': 'h:mm',
+                  'day': 'h:mm',
+                  'week': 'h:mm',
+                  'month': 'h:mm',
+                  'quarter': 'h:mm',
+                  'year': 'h:mm',
+                }
+              }
+            }],
+            yAxes: [{
+              display: true
+            }],
+          }
+        }
+      });
+
+      this.tempChart = new Chart('tempCanvas', {
+        type: 'line',
+        data: {
+          labels: this.date,
+          datasets: [
+            {
+              data: this.temp,
+              borderColor: '#297159',
+              fill: false,
+              label: 'temp'
             }
           ]
         },
         options: {
           legend: {
-            display: false
+            display: true,
+            position: 'right'
           },
           scales: {
             xAxes: [{
+              type: 'time',
+              time: {
+                displayFormats: {
+                  'millisecond': 'h:mm',
+                  'second': 'h:mm',
+                  'minute': 'h:mm',
+                  'hour': 'h:mm',
+                  'day': 'h:mm',
+                  'week': 'h:mm',
+                  'month': 'h:mm',
+                  'quarter': 'h:mm',
+                  'year': 'h:mm',
+                }
+              }
+            }],
+            yAxes: [{
               display: true
+            }],
+          }
+        }
+      });
+
+
+      this.humiChart = new Chart('humiCanvas', {
+        type: 'line',
+        data: {
+          labels: this.date,
+          datasets: [
+            {
+              data: this.humi,
+              borderColor: '#E2E062',
+              fill: false,
+              label: 'humi'
+            }
+          ]
+        },
+        options: {
+          legend: {
+            display: true,
+            position: 'right'
+          },
+          scales: {
+            xAxes: [{
+              type: 'time',
+              time: {
+                displayFormats: {
+                  'millisecond': 'h:mm',
+                  'second': 'h:mm',
+                  'minute': 'h:mm',
+                  'hour': 'h:mm',
+                  'day': 'h:mm',
+                  'week': 'h:mm',
+                  'month': 'h:mm',
+                  'quarter': 'h:mm',
+                  'year': 'h:mm',
+                }
+              }
             }],
             yAxes: [{
               display: true
@@ -59,5 +172,4 @@ export class ChartComponent implements OnInit {
 
     });
   }
-
 }
