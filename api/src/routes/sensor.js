431db22fde0f2d5132cc56832391491de846aba4
Gladkowski <marcingladkowski@gmail.com>
2018-11-21 23:34:29 +0100

    refactor routes for sensor to async

--- api/src/routes/sensor.js
+++ api/src/routes/sensor.js
@@ -1,46 +1,17 @@
-import express from 'express';
+import { Router }  from 'express';
+import sensorController from '../controllers/sensorController';
 import multer from 'multer';
-import fs from 'fs';
-import dateformat from 'dateformat';
-import ip from 'ip';
+import { catchAsync } from '../middlewares/errors';
+import { checkServer } from '../auth/auth';
 
-import Sensor from '../models/Sensor';
-import Config from '../config/config';
+export default () => {
+  const api = Router();
+  const upload = multer();
 
-const upload = multer();
-const router = express.Router();
+  // POST api/sensor
+  api.post('/', checkServer, upload.fields([]), catchAsync(sensorController.create));
+  // GET api/sensor
+  api.get('/', sensorController.list);
 
-router.post('/sensor',  upload.fields([]), function (req, res) {  
-
-  const remoteAddress = req.headers['x-real-ip'] || req.connection.remoteAddress;
-
-  if (ip.isEqual(Config.allow_ip, remoteAddress)) {
-    const SensorData = new Sensor();
-
-    SensorData.p1 = req.body.sensordatavalues[0].value;
-    SensorData.p2 = req.body.sensordatavalues[1].value;
-    SensorData.temp = req.body.sensordatavalues[2].value;
-    SensorData.humi = req.body.sensordatavalues[3].value;
-
-    SensorData.save(function (err) {
-      if (err) { 
-        res.send(err);
-      }
-
-     res.status(200).send({'status': 'ok'});  
-    });
-
-  } else {
-    res.status(401).send({'status': 'Ip address not allowed'});
-  }
-  
-});
-
-
-router.get('/sensor', function (req, res) {
-  Sensor.find({}).sort({'createdDate': -1}).limit(100).exec(function(err, sensor) {
-     res.send(sensor);
-  });
-});
-
-module.exports = router
+  return api;
+}
